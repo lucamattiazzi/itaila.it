8019a82a879a09a2a4038756171ece680fedac25
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-12-09 19:28:28 +0100

    some fixes

--- src/index.ts
+++ src/index.ts
@@ -23,23 +23,25 @@ const overrideItaliaBackup = (
 
 const FOOTERS = {
   it: `
-  <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: blue; color: #00ff44; z-index: 9999; font-size: 12px; padding-top: 10px; padding-bottom: 10px">
+  <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #00f; color: #ff6600; z-index: 9999; font-size: 14px; padding-top: 10px; padding-bottom: 10px">
     <div style="font-size: 28px; font-weigth: bold; text-decoration: underline; padding-bottom: 10px">ATTENZIONE</div>
-    <p>Questo sito non è <a href="http://italia.it" target="_blank" rel="noreferrer noopener">quello ufficiale</a> ma offre la sicurezza di una connessione https come tutti i siti dovrebbero fare nel 2020.</p>
+    <p>Questo sito non è <a href="http://italia.it" style="color: #ffff00" target="_blank" rel="noreferrer noopener">quello ufficiale</a> ma offre la sicurezza di una connessione https come <a href="https://www.france.fr" style="color: #ffff00" target="_blank" rel="noreferrer noopener">tutti</a> i <a href="https://deutschland.de" style="color: #ffff00" target="_blank" rel="noreferrer noopener">siti</a> dovrebbero fare nel 2020.</p>
     <p>Al netto di questo footer (che è intenzionalmente un pugno nell'occhio, inoltre faccio cagare nel design, pace) dovrebbe essere identico a quello ufficiale.</p>
     <p>Serve solo per poter fingere che il dominio principale del nostro paese abbia una delle sicurezze basilari dell'internets.</p>
-    </div>`,
+    <p>Leggi di più sul <a href="https://github.com/lucamattiazzi/itaila.it" style="color: #ffff00">repo Github</a></p>
+  </div>`,
   en: `
-    <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: blue; color: #00ff44; z-index: 9999; font-size: 12px; padding-top: 10px; padding-bottom: 10px">
+  <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #00f; color: #ff6600; z-index: 9999; font-size: 14px; padding-top: 10px; padding-bottom: 10px">
     <div style="font-size: 28px; font-weigth: bold; text-decoration: underline; padding-bottom: 10px">WARNING</div>
-    <p>This website is not the <a href="http://italia.it" target="_blank" rel="noreferrer noopener">official one</a> but offers the safety of an https connection just like every website should do in 2020.</p>
-    <p>Aside from this footer (which sucks design-wise intentionally, and also I cannot do design, sry) it should be identical to the official one.</p>
+    <p>This website is not the <a href="http://italia.it" style="color: #ffff00" target="_blank" rel="noreferrer noopener">official one</a> but offers the safety of an https connection just like <a href="https://www.france.fr" style="color: #ffff00" target="_blank" rel="noreferrer noopener">every</a> <a href="https://deutschland.de" style="color: #ffff00" target="_blank" rel="noreferrer noopener">website</a> should do in 2020.</p>
+    <p>Aside from this footer (which sucks design-wise, intentionally but also I cannot do design, sry) it should be identical to the official one.</p>
     <p>Its only purpose it to pretend that the main domain of our country has one of the most basic security features of the internets.</p>
+    <p>Read more on the <a href="https://github.com/lucamattiazzi/itaila.it" style="color: #ffff00">Github repo</a></p>
   </div>`,
   github: `
-    <a href="https://github.com/lucamattiazzi/itaila.it" style="position: absolute; top: 0; right: 0; z-index:9999">
-      <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_orange_ff7600.png?resize=149%2C149" alt="Fork me on GitHub" data-recalc-dims="1">
-    </a>
+  <a href="https://github.com/lucamattiazzi/itaila.it" style="position: absolute; top: 0; left: 0; z-index:9999">
+    <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_orange_ff7600.png?resize=149%2C149" alt="Fork me on GitHub" data-recalc-dims="1">
+  </a>
   `,
 }
 
@@ -56,6 +58,7 @@ function proxyItalia(req: Request, res: Response) {
     const footer = req.path.startsWith('/it') ? FOOTERS.it : FOOTERS.en
     const htmlWithFooter = `${text}${footer}${FOOTERS.github}`
     cache.set(req.path, htmlWithFooter)
+    res.setHeader('content-type', 'text/html; charset=utf-8')
     res.end(htmlWithFooter)
   })
 }
